220caca0a4b50454e8afbaf0128cf8213502eeee
GuilhermeJaime <guilhermejaime16@gmail.com>
2025-08-19 11:53:35 +0100

    formulário feito

---
+++ exercicios/ex25/cadastro.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP</title>
+  <style>
+    body {
+      font-family: Arial, sans-serif;
+      background-color: #f4f4f4;
+      color: #333;
+      text-align: center;
+      padding: 50px;
+    }
+    h1 {
+      color: #4CAF50;
+      align: center;
+      
+    }
+  </style>
+</head>
+<body>
+  <h1>  Seja muito bem vindo!</h1>
+</body>
+</html>
